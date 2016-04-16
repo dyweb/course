68f0c2ac4d5573a28ec8a1308b60b04d514a3652
ComMouse <hanyikai1996@126.com>
2016-04-16 18:43:06 +0800

    Improve v1

--- web/2016-spring/instant-php/src/v1/order.php
+++ web/2016-spring/instant-php/src/v1/order.php
@@ -3,35 +3,41 @@
 date_default_timezone_set('Asia/Shanghai');
 
 // Fetch form data from query string
-$name = empty($_GET['name']) ? '' : $_GET['name'];  // The old way
-$quantity = $_GET['quantity'] ?? '';                // The new way availbale in PHP 7
+// The old way
+$name = empty($_GET['name']) ? '' : $_GET['name'];
+$quantity = empty($_GET['quantity']) ? '' : $_GET['quantity'];
+
+// The new way availbale in PHP 7
+//$name = $_GET['name'] ?? '';
+//$quantity = $_GET['quantity'] ?? '';
 
 // If the form is submitted
 if ($name != '' && $quantity != '') {
     // Output order result
-    $price = 3;
+    $price = 3.00;
     $total_price = $quantity * $price;
-    echo '<h1>Ticket Ordering</h1>';
-    echo '<p>Your order was created successfully.</p>';
-    echo '<ul>';
-    echo '    <li>Name: ' . $name . '</li>';
-    echo '    <li>Tickets: ' . $quantity . '</li>';
-    echo '    <li>Amount: $' . number_format($total_price, 2) . '</li>';
-    echo '    <li>Ordered at: ' . date('Y-m-d H:i:s') . '</li>';
-    echo '    <li>Order No: ' . sprintf('%010d', $quantity * 10 + 1) . '</li>';
-    echo '</ul>';
-} else {
-    echo '<h1>Ticket Ordering</h1>';
-    echo '<p>You are ordering tickets of the concert <em>Hello World</em>.</p>';
-    echo '<form method="GET" action="order.php">';
-    echo '    <p>';
-    echo '        <label>Name</label>';
-    echo '        <input name="name" type="text" placeholder="Your Name">';
-    echo '    </p>';
-    echo '    <p>';
-    echo '        <label>Ticket Quantity</label>';
-    echo '        <input name="quantity" type="number" placeholder="Quantity" min="0" max="5" style="width: 100px;">';
-    echo '    </p>';
-    echo '    <button>Submit</button>';
-    echo '</form>';
-}
+?>
+    <h1>Ticket Ordering</h1>
+    <p>Your order was created successfully.</p>
+    <ul>
+        <li>Name: <?php echo $name; ?></li>
+        <li>Tickets: <?php echo $quantity; ?></li>
+        <li>Amount: $<?php echo number_format($total_price, 2) ?></li>
+        <li>Ordered at: <?php echo date('Y-m-d H:i:s') ?></li>
+        <li>Order No: <?php echo sprintf('%010d', $quantity * 10 + 1) ?></li>
+    </ul>
+<?php } else { ?>
+    <h1>Ticket Ordering</h1>
+    <p>You are ordering tickets of the concert <em>Hello World</em>.</p>
+    <form method="GET" action="order.php">
+        <p>
+            <label>Name</label>
+            <input name="name" type="text" placeholder="Your Name">
+        </p>
+        <p>
+            <label>Ticket Quantity</label>
+            <input name="quantity" type="number" placeholder="Quantity" min="0" max="5" style="width: 100px;">
+        </p>
+        <button>Submit</button>
+    </form>
+<?php } ?>
